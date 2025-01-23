addf6ae7de2c2b18bf3ee6e984de1c0b141ae649
Dean Putney <putney.dean@gmail.com>
2025-01-22 16:32:41 -0800

    Github Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://idealistsoftware.github.io",
+  base: "stardate_documentation",
   integrations: [
     starlight({
       title: "Stardate",
